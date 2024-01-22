67355b600906065799e711b1c80c6ffac8982804
30IGithub <54778430+Hexagon1296@users.noreply.github.com>
2024-01-22 14:01:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let coll = document.getElementsByClassName("open");
 for(let i = 0;i<coll.length;i++){
  coll[i].addEventListener("onclick", function() {
     this.classList.toggle("collapsed");
+    alert(this.parentElement)
     var content = this.parentElement.nextElementSibling;
     if (content.style.display === "") {
       content.style.display = "none";
