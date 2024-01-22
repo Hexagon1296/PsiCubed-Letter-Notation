efff63b78abe5cd671929b820faedf3a19bc1ed4
30IGithub <54778430+Hexagon1296@users.noreply.github.com>
2024-01-22 15:22:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let coll = document.getElementsByClassName("opened");
 
 for(let i = 0;i<coll.length;i++){
-  coll[i].addEventListener("click", function() {
+  let el = coll[i];
+  let content = el.parentElement;
+  if (content.constructor.name=="HTMLDivElement"){
+    content = el;
+  }
+  content = content.nextElementSibling
+  content.style.maxHeight = content.scrollHeight + "px";
+  el.addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.parentElement;
     if (content.constructor.name=="HTMLDivElement"){
@@ -11,7 +18,12 @@ for(let i = 0;i<coll.length;i++){
     if (content.style.maxHeight!="0px"){
       content.style.maxHeight = "0";
     } else {
-      content.style.maxHeight = "none";
+      height = content.scrollHeight
+      content.style.maxHeight = height + "px";
+      content = content.parentElement
+      while (content.constructor.name!="HTMLDivElement"){
+        content.style.maxHeight = height+(content.style.maxHeight.slice(0,-2)) + "px";
+      }
     }
   });
 }
