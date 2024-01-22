ed5cd02ad334d16e1a92183461e466c41a2d03a7
30IGithub <54778430+Hexagon1296@users.noreply.github.com>
2024-01-22 14:19:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let coll = document.getElementsByClassName("opened");
 
 for(let i = 0;i<coll.length;i++){
- coll[i].addEventListener("click", function() {
+  coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
+    alert(this.parentElement.constructor.name)
     var content = this.parentElement.nextElementSibling;
     if (content.style.maxHeight){
       content.style.maxHeight = null;
