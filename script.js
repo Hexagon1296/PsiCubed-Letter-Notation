75659b9e6d1b0ddd8d191537d11449a8baa36c5a
30IGithub <54778430+Hexagon1296@users.noreply.github.com>
2024-01-22 14:03:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-let coll = document.getElementsByClassName("open");
+let coll = document.getElementsByClassName("opened");
 
 for(let i = 0;i<coll.length;i++){
- coll[i].addEventListener("onclick", function() {
-    this.classList.toggle("collapsed");
-    alert(this.parentElement)
+ coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
     var content = this.parentElement.nextElementSibling;
-    if (content.style.display === "") {
-      content.style.display = "none";
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
     } else {
-      content.style.display = "";
+      content.style.maxHeight = content.scrollHeight + "px";
     }
   });
 }
