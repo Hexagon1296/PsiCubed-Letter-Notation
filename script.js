202d4cb696d66368e83c14fa581eca9241940440
30IGithub <54778430+Hexagon1296@users.noreply.github.com>
2024-01-22 12:57:20 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+let coll = document.getElementsByClassName("open");
+
+for(let i = 0;i<coll.length;i++){
+ coll[i].addEventListener("click", function() {
+    this.classList.toggle("closed");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
