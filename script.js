d489bee330005d0fbb9d717e0959fb7f3adf2803
30IGithub <54778430+Hexagon1296@users.noreply.github.com>
2024-01-22 13:06:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ for(let i = 0;i<coll.length;i++){
  coll[i].addEventListener("click", function() {
     this.classList.toggle("closed");
     var content = this.parentNode.nextElementSibling;
+    document.append(content);
     if (content.style.maxHeight){
       content.style.maxHeight = null;
     } else {
