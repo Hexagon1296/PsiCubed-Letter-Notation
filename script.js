4a178cb76af9004f52349a0d4b58444b47e15a3a
30IGithub <54778430+Hexagon1296@users.noreply.github.com>
2024-01-24 18:31:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let coll = document.getElementsByClassName("collapser");
+let next = document.getElementById("next");
+let cont = document.getElementsByClassName("content")[0];
+next.style.top = next.offsetTop + "px";
 
 for(let i = 0;i<coll.length;i++){
   let el = coll[i];
