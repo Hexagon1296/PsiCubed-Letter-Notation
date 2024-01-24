0637f8902dafc4eaaa2cbb4a2ecaec199224f643
30IGithub <54778430+Hexagon1296@users.noreply.github.com>
2024-01-24 08:36:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 let coll = document.getElementsByClassName("collapser");
-let con = document.getElementsByClassName("content")[0];
-let nav = document.getElementsByClassName("navbar")[0];
-let val = (x) => +(x.slice(0,-2));
-let navWidth = val(window.getComputedStyle(nav).width) + val(window.getComputedStyle(nav).paddingRight) + val(window.getComputedStyle(nav).paddingLeft)
-con.style.width = con.clientWidth - navWidth - val(window.getComputedStyle(con).paddingLeft) + "px";
-con.style.marginLeft = navWidth + "px";
 
 for(let i = 0;i<coll.length;i++){
   let el = coll[i];
