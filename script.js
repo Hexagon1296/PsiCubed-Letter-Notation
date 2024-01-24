d339ecb7f9b6a214292b54275250aa14a4177c16
30IGithub <54778430+Hexagon1296@users.noreply.github.com>
2024-01-24 08:31:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let nav = document.getElementsByClassName("navbar")[0];
 let val = (x) => +(x.slice(0,-2));
 let navWidth = val(window.getComputedStyle(nav).width) + val(window.getComputedStyle(nav).paddingRight) + val(window.getComputedStyle(nav).paddingLeft)
 con.style.width = con.clientWidth - navWidth + "px";
+con.style.marginLeft = navWidth + "px";
 
 for(let i = 0;i<coll.length;i++){
   let el = coll[i];
